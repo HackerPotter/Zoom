53c4958c2dbbb100ee7944668f3cfda17bd40383
HackerPotter <89425208+HackerPotter@users.noreply.github.com>
2021-11-16 15:25:41 -0600

    Agradecimientos

---
+++ README.py
@@ -0,0 +1,5 @@
+# Zoom
+#Ejecuta este programa en un compilador de Python
+def Agradecimientos():
+  print("Agradecimientos a mi amigo quien ayudo pensar en este proyecto escrito en el lenguage de python."
+agradecimientos()
